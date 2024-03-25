71c39900bcbbdb85e2442d2516a1b8e9f3a097d0
Muin-Uddin-Ahmad <muinuddin.sm12@gamil.com>
2024-03-26 02:23:25 +0600

    Navbar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
-import "./App.css";
+import Nav from "./components/Nav";
+
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!!</h1>
+      <Nav/>
     </>
   );
 }
---
+++ src/components/Nav.jsx
@@ -0,0 +1,28 @@
+const Nav = () => {
+  return (
+    <div className="max-w-[1440px] py-10 mx-auto px-[135px] flex justify-between items-center">
+      <a className="text-3xl font-bold">BookVerse</a>
+      <ul className="flex gap-4 items-center text-lg">
+        <li>
+          <a>Home</a>
+        </li>
+        <li>
+          <a>Listed Books</a>
+        </li>
+        <li>
+          <a>Pages to Read</a>
+        </li>
+      </ul>
+      <div className="flex items-center gap-4">
+        <a className="px-4 py-2 rounded-md font-semibold text-lg bg-[#23BE0A] text-white transition duration-300 hover:bg-white hover:text-[#23BE0A] border-[1px] hover:border-[#23BE0A] cursor-pointer">
+          Sign In
+        </a>
+        <a className="px-4 py-2 rounded-md font-semibold text-lg bg-[#59C6D2] text-white transition duration-300 hover:bg-white hover:text-[#59C6D2] border-[1px] hover:border-[#59C6D2] cursor-pointer">
+          Sign Up
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Nav;
