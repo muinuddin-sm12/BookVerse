d66fe57446f94eb10ebb28e18493b39462cc1133
Muin-Uddin-Ahmad <muinuddin.sm12@gamil.com>
2024-03-26 02:35:05 +0600

    Hero section added

---
+++ src/components/Hero.jsx
@@ -0,0 +1,22 @@
+import hero_image from "../assets/images/Hero_image.png";
+const Hero = () => {
+  return (
+    <div className="max-w-[1440px] flex justify-between items-center mx-auto px-4 md:px-[135px] rounded-3xl ">
+      <div className="bg-base-200 w-full flex flex-col-reverse gap-4 md:gap-0 py-6 md:py-0 items-center min-h-[554px] px-6 md:px-28 rounded-3xl">
+        <div className="flex-1">
+          <h1 className="text-3xl md:text-5xl font-bold mb-8">
+            Books to freshen up your bookshelf
+          </h1>
+          <button className="bg-[#23BE0A] px-5 py-2 text-lg font-semibold hover:bg-white hover:text-[#23BE0A] transition duration-300 border-[1px] hover:border-[#23BE0A] text-white rounded-md">
+            View The List
+          </button>
+        </div>
+        <div>
+          <img src={hero_image} alt="" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,8 +1,8 @@
 const Nav = () => {
   return (
-    <div className="max-w-[1440px] py-10 mx-auto px-[135px] flex justify-between items-center">
+    <div className="max-w-[1440px] py-10 mx-auto px-4 md:px-[135px] flex justify-between items-center">
       <a className="text-3xl font-bold">BookVerse</a>
-      <ul className="flex gap-4 items-center text-lg">
+      <ul className="gap-4 hidden lg:flex items-center text-lg">
         <li>
           <a>Home</a>
         </li>
@@ -17,7 +17,7 @@ const Nav = () => {
         <a className="px-4 py-2 rounded-md font-semibold text-lg bg-[#23BE0A] text-white transition duration-300 hover:bg-white hover:text-[#23BE0A] border-[1px] hover:border-[#23BE0A] cursor-pointer">
           Sign In
         </a>
-        <a className="px-4 py-2 rounded-md font-semibold text-lg bg-[#59C6D2] text-white transition duration-300 hover:bg-white hover:text-[#59C6D2] border-[1px] hover:border-[#59C6D2] cursor-pointer">
+        <a className="hidden md:block px-4 py-2 rounded-md font-semibold text-lg bg-[#59C6D2] text-white transition duration-300 hover:bg-white hover:text-[#59C6D2] border-[1px] hover:border-[#59C6D2] cursor-pointer">
           Sign Up
         </a>
       </div>
