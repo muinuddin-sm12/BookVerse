93ce3bbfb68ad8f88b7c9c08aa595454158f8d02
Muin-Uddin-Ahmad <muinuddin.sm12@gamil.com>
2024-03-27 10:22:47 +0600

    (Pages to Read) page barChart added

--- src/pages/PagesToRead.jsx
+++ src/pages/PagesToRead.jsx
@@ -1,10 +1,67 @@
-
+import { useEffect, useState } from "react";
+import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid } from "recharts";
+import { getBooks } from "../utilities/utility";
 const PagesToRead = () => {
-    return (
-        <div>
-            <h1>Pages to read page</h1>
-        </div>
-    );
+  const [books, setBooks] = useState([]);
+  useEffect(() => {
+    const storedBooks = getBooks();
+    setBooks(storedBooks);
+  }, []);
+
+  const colors = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042", "red", "pink"];
+
+  const data = books.map((item) => ({
+    name: item.bookName,
+    uv: item.totalPages,
+    pv: 1398,
+    amt: 2210,
+  }));
+  const getPath = (x, y, width, height) => {
+    return `M${x},${y + height}C${x + width / 3},${y + height} ${
+      x + width / 2
+    },${y + height / 3}
+  ${x + width / 2}, ${y}
+  C${x + width / 2},${y + height / 3} ${x + (2 * width) / 3},${y + height} ${
+      x + width
+    }, ${y + height}
+  Z`;
+  };
+
+  const TriangleBar = (props) => {
+    const { fill, x, y, width, height } = props;
+
+    return <path d={getPath(x, y, width, height)} stroke="none" fill={fill} />;
+  };
+
+  return (
+    <div className="max-w-[1440px] py-10 mx-auto px-4 md:px-[135px]">
+      <BarChart
+        width={900}
+        height={500}
+        data={data}
+        margin={{
+          top: 20,
+          right: 30,
+          left: 20,
+          bottom: 5,
+        }}
+      >
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="name" />
+        <YAxis />
+        <Bar
+          dataKey="uv"
+          fill="#8884d8"
+          shape={<TriangleBar />}
+          label={{ position: "top" }}
+        >
+          {data.map((entry, index) => (
+            <Cell key={`cell-${index}`} fill={colors[index % 20]} />
+          ))}
+        </Bar>
+      </BarChart>
+    </div>
+  );
 };
 
 export default PagesToRead;
